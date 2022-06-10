1221d6817ee9248d897876b88839f4f3ecb005ff
Elementary1092 <pdead6223@gmail.com>
2022-06-10 22:38:12 +0500

    Add save handler

--- internal/api/handlers/crud/getHandler.go
+++ internal/api/handlers/crud/getHandler.go
@@ -1,117 +1,117 @@
 package crud
 
 import (
-    "context"
-    "encoding/json"
-    "errors"
-    "github.com/elem1092/gateway/internal/domain"
-    services "github.com/elem1092/gateway/pkg/client/grpc/CRUDService"
-    "github.com/elem1092/gateway/pkg/logging"
-    "github.com/gorilla/mux"
-    "io"
-    "net/http"
-    "strconv"
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/elem1092/gateway/internal/domain"
+	services "github.com/elem1092/gateway/pkg/client/grpc/CRUDService"
+	"github.com/elem1092/gateway/pkg/logging"
+	"github.com/gorilla/mux"
+	"io"
+	"net/http"
+	"strconv"
 )
 
 var (
-    ErrNoUserIdField = errors.New("could not find user_id field in request body")
+	ErrNoUserIdField = errors.New("could not find user_id field in request body")
 )
 
 type getHandler struct {
-    logger *logging.Logger
-    client services.CRUDServiceClient
+	logger *logging.Logger
+	client services.CRUDServiceClient
 }
 
 func NewGetHandler(logger *logging.Logger, client services.CRUDServiceClient) http.Handler {
-    return &getHandler{
-        logger: logger,
-        client: client,
-    }
+	return &getHandler{
+		logger: logger,
+		client: client,
+	}
 }
 
 // GET /posts?id=   {id_type: '...'} id_type may be 'USER_ID', 'POST_ID'
 func (g *getHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-    writer.Header().Set("Content-Type", "text/json")
-    g.logger.Info("Processing GET ALL POSTS BY ID request")
+	writer.Header().Set("Content-Type", "text/json")
+	g.logger.Info("Processing GET ALL POSTS BY ID request")
 
-    vars := mux.Vars(request)
-    idStr, ok := vars["id"]
-    if !ok {
-        writer.WriteHeader(http.StatusBadRequest)
-        g.logger.Errorf("Could not find id variable on: %s", request.URL.String())
-        return
-    }
+	vars := mux.Vars(request)
+	idStr, ok := vars["id"]
+	if !ok {
+		writer.WriteHeader(http.StatusBadRequest)
+		g.logger.Errorf("Could not find id variable on: %s", request.URL.String())
+		return
+	}
 
-    encoder := json.NewEncoder(writer)
-    id, err := strconv.ParseInt(idStr, 10, 32)
-    idType, err := g.parseIdType(request)
-    if err != nil {
-        writer.WriteHeader(http.StatusBadRequest)
-        errMsg := domain.ErrorDTO{Error: err.Error()}
-        if err = encoder.Encode(errMsg); err != nil {
-            g.logger.Errorf("Error while encoding json: %v", err)
-        }
-        return
-    }
+	encoder := json.NewEncoder(writer)
+	id, err := strconv.ParseInt(idStr, 10, 32)
+	idType, err := g.parseIdType(request)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		errMsg := domain.ErrorDTO{Error: err.Error()}
+		if err = encoder.Encode(errMsg); err != nil {
+			g.logger.Errorf("Error while encoding json: %v", err)
+		}
+		return
+	}
 
-    req := &services.GetPostsRequest{
-        Id:     int32(id),
-        Needed: idType,
-    }
-    client, err := g.client.GetPosts(context.Background(), req)
-    if err != nil {
-        g.logger.Errorf("Got error while getting all messages: %v", err)
-        writer.WriteHeader(http.StatusInternalServerError)
-        errMsg := domain.ErrorDTO{Error: err.Error()}
-        if err = encoder.Encode(errMsg); err != nil {
-            g.logger.Errorf("Error while encoding error message: %v", err)
-        }
-        return
-    }
+	req := &services.GetPostsRequest{
+		Id:     int32(id),
+		Needed: idType,
+	}
+	client, err := g.client.GetPosts(context.Background(), req)
+	if err != nil {
+		g.logger.Errorf("Got error while getting all messages: %v", err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		errMsg := domain.ErrorDTO{Error: err.Error()}
+		if err = encoder.Encode(errMsg); err != nil {
+			g.logger.Errorf("Error while encoding error message: %v", err)
+		}
+		return
+	}
 
-    for {
-        post, err := client.Recv()
-        if err != nil {
-            if errors.Is(err, io.EOF) {
-                break
-            }
+	for {
+		post, err := client.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
 
-            g.logger.Errorf("Got error while parsing posts: %v", err)
-            return
-        }
+			g.logger.Errorf("Got error while parsing posts: %v", err)
+			return
+		}
 
-        if err = encoder.Encode(post); err != nil {
-            g.logger.Errorf("Got error while encoding post: %v", err)
-            writer.WriteHeader(http.StatusInternalServerError)
-            return
-        }
-    }
+		if err = encoder.Encode(post); err != nil {
+			g.logger.Errorf("Got error while encoding post: %v", err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
 
-    writer.WriteHeader(http.StatusOK)
+	writer.WriteHeader(http.StatusOK)
 }
 
 func (g *getHandler) parseIdType(r *http.Request) (services.GetPostsRequest_IdType, error) {
-    g.logger.Info("Processing request body")
+	g.logger.Info("Processing request body")
 
-    decoder := json.NewDecoder(r.Body)
-    body := make(map[string]string)
-    if err := decoder.Decode(body); err != nil {
-        g.logger.Errorf("Got error while decoding body: %v", err)
-        return -1, err
-    }
+	decoder := json.NewDecoder(r.Body)
+	body := make(map[string]string)
+	if err := decoder.Decode(body); err != nil {
+		g.logger.Errorf("Got error while decoding body: %v", err)
+		return -1, err
+	}
 
-    idTypeFromUser, ok := body["id_type"]
-    if !ok {
-        g.logger.Error(ErrNoUserIdField.Error())
-        return -1, ErrNoUserIdField
-    }
+	idTypeFromUser, ok := body["id_type"]
+	if !ok {
+		g.logger.Error(ErrNoUserIdField.Error())
+		return -1, ErrNoUserIdField
+	}
 
-    g.logger.Info("Determining user_id type")
-    idType, ok := services.GetPostsRequest_IdType_value[idTypeFromUser]
-    if !ok {
-        g.logger.Errorf("Invalid user_id: %s", idTypeFromUser)
-        return -1, errors.New("Invalid user_id: " + idTypeFromUser)
-    }
+	g.logger.Info("Determining user_id type")
+	idType, ok := services.GetPostsRequest_IdType_value[idTypeFromUser]
+	if !ok {
+		g.logger.Errorf("Invalid user_id: %s", idTypeFromUser)
+		return -1, errors.New("Invalid user_id: " + idTypeFromUser)
+	}
 
-    return services.GetPostsRequest_IdType(idType), nil
+	return services.GetPostsRequest_IdType(idType), nil
 }
--- internal/api/handlers/crud/saveHandler.go
+++ internal/api/handlers/crud/saveHandler.go
@@ -1,31 +1,73 @@
 package crud
 
 import (
-	services "github.com/elem1092/gateway/pkg/client/grpc/CRUDService"
-	"github.com/elem1092/gateway/pkg/logging"
-	"net/http"
+    "context"
+    "encoding/json"
+    "github.com/elem1092/gateway/internal/domain"
+    services "github.com/elem1092/gateway/pkg/client/grpc/CRUDService"
+    "github.com/elem1092/gateway/pkg/logging"
+    "net/http"
 )
 
 type saveHandler struct {
-	logger *logging.Logger
-	client services.CRUDServiceClient
+    logger *logging.Logger
+    client services.CRUDServiceClient
 }
 
 func NewSaveHandler(logger *logging.Logger, client services.CRUDServiceClient) http.Handler {
-	return &saveHandler{
-		logger: logger,
-		client: client,
-	}
+    return &saveHandler{
+        logger: logger,
+        client: client,
+    }
 }
 
 // POST /posts {user_id: ..., title: '...', body: '...'}
 func (s *saveHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "text/json")
-	//TODO implement me
-	panic("implement me")
+    writer.Header().Set("Content-Type", "text/json")
+
+    encoder := json.NewEncoder(writer)
+
+    newPost, err := s.parseBodyToSaveDTO(request)
+    if err != nil {
+        writer.WriteHeader(http.StatusBadRequest)
+        errMsg := domain.ErrorDTO{Error: err.Error()}
+        if err = encoder.Encode(errMsg); err != nil {
+            s.logger.Errorf("Error while encoding json: %v", err)
+        }
+        return
+    }
+
+    savedPost, err := s.client.SavePost(context.Background(), newPost)
+    if err != nil {
+        writer.WriteHeader(http.StatusInternalServerError)
+        errMsg := domain.ErrorDTO{Error: err.Error()}
+        if err = encoder.Encode(errMsg); err != nil {
+            s.logger.Errorf("Error while encoding json: %v", err)
+        }
+        return
+    }
+
+    writer.WriteHeader(http.StatusOK)
+    if err := encoder.Encode(savedPost); err != nil {
+        s.logger.Errorf("Error while encoding response: %v", err)
+    }
 }
 
 func (s *saveHandler) parseBodyToSaveDTO(r *http.Request) (*services.SavePostDTO, error) {
+    s.logger.Info("Parsing request body")
+    decoder := json.NewDecoder(r.Body)
 
-	return nil, nil
+    newPost := &domain.SavePostDTO{}
+    if err := decoder.Decode(newPost); err != nil {
+        s.logger.Errorf("error while decoding request body: %v", err)
+        return nil, err
+    }
+
+    return &services.SavePostDTO{
+        UserId: newPost.UserId,
+        Content: &services.ContentDTO{
+            Title: newPost.Title,
+            Body:  newPost.Body,
+        },
+    }, nil
 }
